f96cd47f18534e279a03d396209e3666d70ea83c
Miles Eastwood <meastwood@spartaglobal.com>
2019-10-08 16:00:00 +0100

    Designed tests. They failed, Wrote code. Test passed, refactored until test could no longer be cheated

--- test_unittest_simplecalc.py
+++ test_unittest_simplecalc.py
@@ -7,13 +7,18 @@ class Calctest(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(2,4), 6)
+        self.assertEqual(self.calc.add(400,600), 1000)
 
     def test_subract(self):
         self.assertEqual(self.calc.subtract(6,4), 2)
+        self.assertEqual(self.calc.subtract(55,30), 25)
 
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(2,2), 4)
+        self.assertEqual(self.calc.multiply(4, 5), 20)
+
 
     def test_divide(self):
         #   cal.divide(10,2) == 5
         self.assertEqual(self.calc.divide(10,2), 5)
+        self.assertEqual(self.calc.divide(10, 10), 1)
